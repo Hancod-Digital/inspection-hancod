58202b22397f3b7f3bbc4faea6573239c258cede
Anshad-KT <anshadkt78@gmail.com>
2024-11-08 18:27:46 +0530

    fix: added in bugs and CR

--- src/app/(app)/(main-page)/print-cards/_components/AddProfile.tsx
+++ src/app/(app)/(main-page)/print-cards/_components/AddProfile.tsx
@@ -519,10 +519,11 @@ export default function UserForm({ onClose, setChanged, changed }: UserFormProps
               <div className="bottom-0 left-0 p-4 text-black text-sm font-semibold">
   <ul className="list-disc list-inside">
     <li>Post the details, click on the submit button</li>
-    <li>After posting the details, click on the 'User details' tab to display user details.</li>
-    <li>And click on the 'Generate QR-Code' button to create a QR code</li>
+    <li>After posting the details, click on the &apos;User details&apos; tab to display user details.</li>
+    <li>And click on the &apos;Generate QR-Code&apos; button to create a QR code</li>
   </ul>
 </div>
+
 
               <div className="flex w-full justify-end pt-20 gap-4">
                 <Button type="reset" className="px-10" onClick={onClose} variant="outline">
