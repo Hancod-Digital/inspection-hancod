3ad449a2774a921ff4acae9c7d6d12cf36b736d6
Anshad-KT <anshadkt78@gmail.com>
2024-12-26 12:45:35 +0530

    initial commit

--- src/app/(app)/(main-page)/transactions/(subtopics)/lifting-gear-multi/page.tsx
+++ src/app/(app)/(main-page)/transactions/(subtopics)/lifting-gear-multi/page.tsx
@@ -146,10 +146,10 @@ const LiftingGearMulti = () => {
 
 
                     >
-                        {isLocation && <Location onClose={()=>setIsLocation(false)} setIsSite={setIsSite} setIsArea={setIsArea} setIsChanged={setIsChanged} isChanged={isChanged}/>}
+                        {/* {isLocation && <Location onClose={()=>setIsLocation(false)} setIsSite={setIsSite} setIsArea={setIsArea} setIsChanged={setIsChanged} isChanged={isChanged}/>}
                         {isEquipment && <Equipment  changed={changed} onClose={() => setIsEquipment(false)} setIsManufacturer={setIsManufacturer} setIsStandard={setIsStandard} setIsLocation={setIsLocation} isManufacturer={isManufacturer} isStandard={isStandard} isLocation={isLocation} minorCategoryOptions={minorCategoryOptions} supplierOptions={supplierOptions} standardOptions={standardOptions} annexureOptions={annexureOptions} locationOptions={locationOptions} ownerOptions={ownerOptions} />}
                         {isStandard && <Standard onClose={()=>setIsStandard(false)} />}
-                        {isManufacturer && <Manufacturer onClose={()=>setIsManufacturer(false)} />}
+                        {isManufacturer && <Manufacturer onClose={()=>setIsManufacturer(false)} />} */}
                         {!isLocation && !isEquipment && !isStandard && !isManufacturer && (
                         <AddForm onClose={handleCloseAdd} setIsLocation={setIsLocation} setIsEquipment={setIsEquipment} setIsStandard={setIsStandard} setIsManufacturer={setIsManufacturer} />
                     )}
--- src/app/(app)/(main-page)/transactions/(subtopics)/lifting-gear-single/_components/Table.tsx
+++ src/app/(app)/(main-page)/transactions/(subtopics)/lifting-gear-single/_components/Table.tsx
@@ -325,10 +325,10 @@ document.head.removeChild(styleElement);
                                         transition={{ duration: 0.2 }}
                                     > 
                                         <TableCell colSpan={5}>
-                                            {isLocation && <Location onClose={()=>setIsLocation(false)} setIsSite={setIsSite} setIsArea={setIsArea} />}
+                                            {/* {isLocation && <Location onClose={()=>setIsLocation(false)} setIsSite={setIsSite} setIsArea={setIsArea} />}
                                             {isEquipment && <Equipment  changed={changed} onClose={()=>setIsEquipment(false)} setIsManufacturer={setIsManufacturer} setIsStandard={setIsStandard} setIsLocation={setIsLocation} isManufacturer={isManufacturer} isStandard={isStandard} isLocation={isLocation} minorCategoryOptions={minorCategoryOptions} supplierOptions={supplierOptions} standardOptions={standardOptions} annexureOptions={annexureOptions} locationOptions={locationOptions} ownerOptions={ownerOptions} />}
                                             {isStandard && <Standard onClose={()=>setIsStandard(false)} />}
-                                            {isManufacturer && <Manufacturer onClose={()=>setIsManufacturer(false)} />}
+                                            {isManufacturer && <Manufacturer onClose={()=>setIsManufacturer(false)} />} */}
 
                                         {!isLocation && !isEquipment && !isStandard && !isManufacturer && <EditPopup onClose={handleCloseEdit} id={item?.id} setIsLocation={setIsLocation} setIsEquipment={setIsEquipment} setIsStandard={setIsStandard} setIsManufacturer={setIsManufacturer} />}
                                         </TableCell>
